d21ab0c908dd951b9f4b2450f1e611081c94b1d2
terryvilver <vilver.terry@gmail.com>
2020-10-13 23:51:40 +0200

    Hello World !

---
+++ php/hello.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Hello</title>
+    </head>
+
+    <body>
+
+        <?php 
+            $test= 5;
+
+            echo "Hello World !";
+        ?>
+    </body>
+</html>
