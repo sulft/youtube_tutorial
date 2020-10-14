a0069d46e43878384c004529b676a772c7bd4b0e
terryvilver <vilver.terry@gmail.com>
2020-10-14 20:35:18 +0200

    creating function

---
+++ php/functions/create_first_function.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Hello</title>
+    </head>
+
+    <body>
+
+        <?php
+            function ma_fonction () {
+                echo "Hello World !";
+            }
+
+            ma_fonction();
+            ma_fonction();
+            ma_fonction();
+        ?>
+    </body>
+</html>
