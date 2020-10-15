6a79cc7d1a395cea8b202c5e2d50f2db6b983f05
terryvilver <vilver.terry@gmail.com>
2020-10-15 21:26:15 +0200

    function learning

---
+++ php/functions/includes/head.php
@@ -0,0 +1,11 @@
+<?php 
+    include 'functions.php'
+?>
+
+<html>
+<head>
+    <meta charset ="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
---
+++ php/functions/index.php
@@ -0,0 +1,12 @@
+<?php 
+    include 'includes/head.php';
+    include_once 'includes/header.php';
+?>
+
+<h1><?php introduction(); ?></h1>
+
+<?php 
+    include 'includes/footer.php';
+    //include - path is wrong - we only get a warning
+    //require - path is wrong - fatal error page doesn't load 
+?>
