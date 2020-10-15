03d6f37203267dbd67e246e1fa4cffcf51f08631
terryvilver <vilver.terry@gmail.com>
2020-10-15 22:14:51 +0200

    année bissextile

---
+++ php/functions/exercice/exercice.php
@@ -0,0 +1,36 @@
+<?php 
+    require 'functions.php'
+?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Exercice</title>
+    </head>
+
+    <body>
+        <?php 
+            echo "Exercice 1 : Vat calculator <br><br>";
+
+            $price = 12;
+            const VAT = 0.1;
+
+            echo "Price is : $price$ <br>";
+            echo "Vat is : " . VAT ."<br>";
+            echo "Total price is : " . calculate_vat($price, VAT) ."$<br>";
+        ?>
+
+        <?php 
+            echo "<br><br>Exercice 2 : Leap year <br><br>";
+
+            $year = 2001;
+            if(isLeapYear($year)) {
+                echo $year . " is a leap year.<br>";
+            }
+            else {
+                echo $year . " is not a leap year.<br>";
+            }
+        ?>
+    </body>
+</html>
+
---
+++ php/functions/exercice/functions.php
@@ -0,0 +1,26 @@
+<?php 
+    function calculate_vat($price, $vat) {
+        $calculated_price = ( $price * $vat ) + $price;
+        return $calculated_price;
+    }
+
+    function isLeapYear($year) {
+        if(($year%4)==0) {
+            if(($year/100)) {
+                if(($year/400)) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            return false;
+        }
+        
+    }
+?>
