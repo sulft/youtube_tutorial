6cdc74a0ae33e762622ff487e4fb54c815420277
terryvilver <vilver.terry@gmail.com>
2020-10-16 21:32:30 +0200

    math function

---
+++ php/built_in_function/math_function.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>math function</title>
+    </head>
+
+    <body>
+        <?php
+            echo ceil(9.4) . "<br>"; //arrondit le nombre/variable à l'inférieur
+            echo floor(9.4) . "<br>"; //arrondir le nombre/variable au supérieur
+            echo round(10.29,1) . "<br>"; /*affiche le nombre/variable de chiffre après la virgule du 
+            second argument tout en arrondissant le nombre final */
+            echo exp(0) . "<br>"; //exposant du nombre/variable entre la parenthèse
+        ?> 
+    </body>
+<html>
