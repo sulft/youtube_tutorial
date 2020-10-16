503dba1d38ab2756600282575ee3cff4cd388d3e
terryvilver <vilver.terry@gmail.com>
2020-10-17 00:59:46 +0200

    date/time function

---
+++ php/built_in_function/date_function.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>math function</title>
+    </head>
+
+    <body>
+        <h1>Date function</h1>
+        <?php
+            date_default_timezone_set("America/Regina"); /* attribut le fuseau horraire des Etats unis */
+            echo date('d/m/y');
+            echo "<br>";
+            echo date('l d F Y');
+            echo "<br>";
+            echo date('g:iA');
+        ?> 
+    </body>
+<html>
