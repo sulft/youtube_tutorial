ad0a5e5d01d52ddc841625ca84be9124d7ce44a3
terryvilver <vilver.terry@gmail.com>
2020-10-19 23:12:35 +0200

    Superglobals

--- php/SUPERGLOBALS/files_superglobals.php
+++ php/SUPERGLOBALS/files_superglobals.php
@@ -1,25 +1,12 @@
 <!DOCTYPE html>
-
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>$_FILES</title>
-    </head>
+<body>
 
-    <body>
-        <h1>$_FILES</h1>
-        <?php
-            error_reporting(E_ERROR | E_PARSE);
-            //enctype="multipart/form-data" dit comment les données du formulaire doivent être encodé
-            echo $name = $_FILES['file']['name'] . "<br>";
-            echo $type = $_FILES['file']['type'] . "<br>";
-            echo $error = $_FILES['file']['error'] . "<br>";          
-        ?> 
+<form action="upload.php" method="post" enctype="multipart/form-data">
+  Selectionner une image a charger
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Upload Image" name="submit">
+</form>
 
-        
-        <form action = "files_superglobals.php" method = "post" enctype="multipart/form-data">
-            <input type='file' name='file'>
-            <button type="submit">SUBMIT</button>
-        </form>
-    </body>
-<html>
+</body>
+</html>
